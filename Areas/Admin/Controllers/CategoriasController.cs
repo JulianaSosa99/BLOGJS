2a51f29034303069e2c67628048cb9505ae8048f
Kamila <juliana.sosa@udla.edu.ec>
2024-05-29 09:53:50 -0500

    Agregar archivos de proyecto.

---
+++ Areas/Admin/Controllers/CategoriasController.cs
@@ -0,0 +1,42 @@
+﻿using BlogSocialUdla.AccesoDatos.Data.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogSocialUdla.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoriasController : Controller
+    {
+        private readonly IContenedorTrabajo _contenedorTrabajo;
+        public CategoriasController(IContenedorTrabajo contenedorTrabajo)
+        {
+            _contenedorTrabajo = contenedorTrabajo;
+
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+
+        }
+        /* [HttpPost]
+         public IActionResult Create()
+         {
+
+
+         }*/
+        #region llamadas a la API
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Json(new {data=_contenedorTrabajo.Categoria.GetAll()});
+
+        }
+        #endregion
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+using BlogSocialUdla.AccesoDatos.Data.Repository;
+using BlogSocialUdla.AccesoDatos.Data.Repository.IRepository;
+using BlogSocialUdla.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("ConexionDB") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddControllersWithViews();
+
+//AGREGAT CONTENEDOR DE TRABAJO AL CONTENEDOR IoC de inyección de dependencia, para poder usarlo debo se tiene que reconocer
+builder.Services.AddScoped<IContenedorTrabajo, ContendorTrabajo>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{area=Usuario}/{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+app.Run();
